700a26546718ab3b00c11b335af89ece0a9f43b0
morganaleister <lmraddmb@gmail.com>
2020-07-01 03:14:51 -0500

    Project started
    added gitignore and cs source code just started

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Books_tools
+{
+    class Program
+    {
+        struct Book
+        {
+            string s_title, s_author;
+            bool b_read;
+
+            public Book(string title, string author = "n/d", bool read = false)
+            {
+                s_title = title;
+                s_author = author;
+                b_read = read;
+            }
+        }
+        struct Owner
+        {
+            string s_fname, s_lname;
+            public Owner(string names, string lastNames = "")
+            {
+                s_fname = names;
+                s_lname = lastNames;
+            }
+        }
+
+        Book[] books;
+        string[] titles, fnames,lnames;
+        bool[] states;
+        int[] ppages;
+
+        static void Main(string[] args)
+        {
+            ReadFile();
+
+        }
+
+        private static void ReadFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void GiveRandom()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Books-tools
+ A very simple to use, c#-written console app to organize a book database along with some utilities likes a random selection and filtered search with parameters
