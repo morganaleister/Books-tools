0cdeabbb5e58cc2da3d3aac179fa5c76930bb845
morganaleister <lmraddmb@gmail.com>
2020-07-01 05:15:08 -0500

    json dbs updtd
    +program updated

--- Program.cs
+++ Program.cs
@@ -4,40 +4,106 @@ namespace Books_tools
 {
     class Program
     {
-        struct Book
+        class Book
         {
-            string s_title, s_author;
+            string s_title, s_authorF, s_authorL, s_format;
             bool b_read;
+            int i_pages;
+            Owner o_owner;
 
-            public Book(string title, string author = "n/d", bool read = false)
+            public Book()
+            {
+                throw new InvalidOperationException("This is an invalid instance creation. Please use any of the other constructor methods.");
+            }
+
+            public Book(string title)
+            {
+                new Book(title, "Físico");
+            }
+
+            public Book(string title, string format)
             {
                 s_title = title;
-                s_author = author;
-                b_read = read;
+                s_authorF = "nd";
+                s_authorL = "nd";
+                s_format = format;
+                b_read = false;
+                i_pages = 0;
             }
         }
-        struct Owner
+        class Owner
         {
-            string s_fname, s_lname;
-            public Owner(string names, string lastNames = "")
+            string s_fname, s_lname, s_nick;
+            pref _pref;
+            public enum pref
             {
-                s_fname = names;
-                s_lname = lastNames;
+                FNames,
+                LNames,
+                Nick
+            }
+
+            public Owner()
+            {
+                throw new InvalidOperationException("This is an invalid instance creation. Please use any of the other constructor methods.");
+            }
+
+            public Owner(string name, pref prefDisplay)
+            {
+                s_fname = name;
+                s_lname = "";
+                s_nick = "";
+                _pref = prefDisplay;
             }
         }
 
         Book[] books;
-        string[] titles, fnames,lnames;
-        bool[] states;
-        int[] ppages;
+        Owner[] owners;
 
         static void Main(string[] args)
         {
-            ReadFile();
+            Welcome();
+            ReadFiles();
 
         }
 
-        private static void ReadFile()
+        private static void Welcome()
+        {
+            Console.WriteLine("Bienvenid@!!");
+            Console.WriteLine("El programa está cargando la base de datos, espera un momento por favor...");
+            
+
+        }
+
+        private static void ReadFiles()
+        {
+            ReadBooks();
+            ReadOwners();
+            
+        }
+
+        /// <summary>
+        /// Returns a [LastName][comma][space][FirstName][period] string
+        /// </summary>
+        /// <returns>string. [LastName][comma][space][FirstName][period]</returns>
+        public static string GetFullNameAuthor(string firstName, string lastName)
+        {
+            return string.Format("{0}, {1}.", lastName, firstName);
+        }
+        /// <summary>
+        /// Returns a [FirstName][space][LastName] string
+        /// </summary>
+        /// <returns>string. [FirstName][space][LastName]</returns>
+        public static string GetFullNameOwner(string firstName, string lastName)
+        {
+            return string.Format("{0} {1}", firstName, lastName);
+        }
+
+        private static void ReadOwners()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void ReadBooks()
         {
             throw new NotImplementedException();
         }
