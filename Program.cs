6935e8c4a79afbffaad5a97ab3baea2c5ccc815b
morganaleister <lmraddmb@gmail.com>
2020-07-04 03:28:12 -0500

    more progress

--- Program.cs
+++ Program.cs
@@ -12,12 +12,22 @@ namespace Books_tools
         private static readonly string langs_path = default_path + "langs\\";
         private static readonly string default_language = "ES-MX";
         private static readonly string[] default_commands
-            = new string[] { "exit", "help", "titles" };
+            = new string[] { "exit", "help", "list", "filter", "edit", "settings" };
 
         private static string lang;
         private static string[] langs;
         private static string[] Lines;
         private static string[][] Commands;
+
+        private enum IssuableCommands
+        {
+            exit, 
+            help,
+            list,
+            filter, 
+            edit, 
+            settings
+        }
         
 
         private static Book[] books;
@@ -31,34 +41,11 @@ namespace Books_tools
             WaitForInput();
 
         }
-
-        private static void WaitForInput()
-        {
-            string issuedCmd = Console.ReadLine();
-
-
-        }
-
         private static void Awake()
         {
             SettingsCheck();
             InitializeProgram();
         }
-
-        private static void InitializeProgram()
-        {
-            JsonSerializer serializer = new JsonSerializer();
-            ///Reads settings.json and set program initial settings
-            using (FileStream fs = File.OpenRead(settings_file))
-            using (TextReader sr = new StreamReader(fs))
-            using (JsonReader jsonReader = new JsonTextReader(sr)) 
-            {
-                
-                SetLangs(serializer, jsonReader);
-                SetCommands(serializer,jsonReader);
-            }
-        }
-
         /// <summary>
         /// Checks out for the system directories and files to exist and creates them if needed.
         /// </summary>
@@ -70,9 +57,7 @@ namespace Books_tools
             ///Check for languages directory to exist, and creates one if not along with default language file
             if (!Directory.Exists(langs_path))
             {
-                Directory.CreateDirectory(langs_path);
-
-                
+                Directory.CreateDirectory(langs_path);                
 
                 //create and write default language json file
                 using (FileStream fs = File.OpenWrite(langs_path + "ES-MX.json"))
@@ -140,6 +125,41 @@ namespace Books_tools
 
             
         }
+        private static void WriteLangs(JsonWriter jsonWriter)
+        {
+            string[] langspaths = Directory.GetFiles(langs_path);
+
+            for (int i = 0; i < langspaths.Length; i++)
+            {
+                jsonWriter.WriteValue(Path.GetFileNameWithoutExtension(langspaths[i]));
+            }
+
+        }
+        private static void WriteCommands(JsonWriter jsonWriter)
+        {
+            for (int cmdNo = 0; cmdNo < default_commands.Length; cmdNo++)
+            {
+                jsonWriter.WriteStartObject();
+                jsonWriter.WritePropertyName(default_commands[cmdNo]);
+                jsonWriter.WriteStartArray();
+                //empty array []
+                jsonWriter.WriteEndArray();
+                jsonWriter.WriteEndObject();
+            }
+        }
+        private static void InitializeProgram()
+        {
+            JsonSerializer serializer = new JsonSerializer();
+            ///Reads settings.json and set program initial settings
+            using (FileStream fs = File.OpenRead(settings_file))
+            using (TextReader sr = new StreamReader(fs))
+            using (JsonReader jsonReader = new JsonTextReader(sr))
+            {
+
+                SetLangs(serializer, jsonReader);
+                SetCommands(serializer, jsonReader);
+            }
+        }
         /// <summary>
         /// Initializes Program's Lang from settings
         /// </summary>
@@ -155,34 +175,36 @@ namespace Books_tools
                             jr.Read();
                             lang = serializer.Deserialize<string>(jr);
 
+                            SetLines();
+
                             break;
                         case "available_langs":
 
                             jr.Read();
                             langs = serializer.Deserialize<string[]>(jr);
-                            return; //interrupts the While loop and terminates SetLangs Method
+                            return;
                     }
                 }
             }
-            
-
-            ///Set langStrings Array from the determined language
+        }
+        private static void SetLines()
+        {
+            ///Set Lines Array from the determined language
             using (FileStream fs = File.OpenRead(langs_path + lang + ".json"))
             using (TextReader sr = new StreamReader(fs))
-            using (JsonReader jr2 = new JsonTextReader(sr))
+            using (JsonReader jr = new JsonTextReader(sr))
             {
                 List<string> strList = new List<string>();
-                while (jr2.Read())
+                while (jr.Read())
                 {
-                    if(jr2.TokenType == JsonToken.String)
+                    if (jr.TokenType == JsonToken.String)
                     {
-                        strList.Add((string)jr2.Value);
+                        strList.Add((string)jr.Value);
                     }
                 }
                 Lines = strList.ToArray();
             }
         }
-
         /// <summary>
         /// Initilalizes Program's Commands from settings
         /// </summary>
@@ -217,81 +239,15 @@ namespace Books_tools
                     } while (cmdcnt < default_commands.Length);
 
                     Commands = tempCmds.ToArray();
+                    return;
 
                 }
             }
-        }
-
-  
-        private static void WriteCommands(JsonWriter jsonWriter)
-        {
-            for (int cmdNo = 0; cmdNo < default_commands.Length; cmdNo++)
-            {
-                jsonWriter.WriteStartObject();
-                jsonWriter.WritePropertyName(default_commands[cmdNo]);
-                jsonWriter.WriteStartArray();
-                    //empty array []
-                jsonWriter.WriteEndArray();
-                jsonWriter.WriteEndObject();
-            }            
-        }
-
-        private static void WriteLangs(JsonWriter jsonWriter)
-        {
-            string[] langspaths = Directory.GetFiles(langs_path);
-
-            for (int i = 0; i < langspaths.Length; i++)
-            {
-                jsonWriter.WriteValue(Path.GetFileNameWithoutExtension(langspaths[i]));
-            }
-            
-        }
-
-        private static void UpdateLangs()
-        {
-            JsonSerializer serializer = new JsonSerializer();
-
-            using (FileStream fs = File.Open(settings_file, FileMode.Open, 
-                FileAccess.ReadWrite, FileShare.None))
-            using (TextReader tr = new StreamReader(fs))
-            using (TextWriter tw = new StreamWriter(fs)) 
-            using(JsonReader jr = new JsonTextReader(tr))
-            using (JsonWriter jw = new JsonTextWriter(tw)) 
-            {
-
-                while (jr.Read())
-                {
-                    if ((string)jr.Value == "available_langs")
-                    {
-                        jr.Read();
-                        
-                    }
-                }
-
-
-            }
-        }
-
-
-        private static void PrintInstructions()
-        {
-            for (int i = 6; i < 10; i++)
-            {
-                Console.WriteLine(Lines[i]);
-            }
-        }
-
-        private static void ClearLines(int LinesToClear = 1)
-        {
-            int currentLineCursor = Console.CursorTop - LinesToClear;
-            Console.SetCursorPosition(0, Console.CursorTop);
-            Console.Write(new string(' ', Console.WindowWidth));
-            Console.SetCursorPosition(0, currentLineCursor);
         }
 
         private static void Welcome()
         {
-            Console.WriteLine(Lines[0]);
+            Console.WriteLine(Lines[0] + lang);
             Console.WriteLine(Lines[1]);
         }
 
@@ -332,6 +288,65 @@ namespace Books_tools
 
             books = booklist.ToArray();
         }
+        private static void PrintInstructions()
+        {
+            for (int i = 6; i < Lines.Length; i++)
+            {
+                Console.WriteLine(Lines[i]);
+            }
+            Console.WriteLine("\nIssue Command:");
+        }
+        private static void WaitForInput()
+        {
+            IssuableCommands issuedCmd;
+            string preCmd;
+
+            do
+            {
+                preCmd = Console.ReadLine();
+
+                
+
+            } while (issuedCmd != IssuableCommands.exit);
+
+             = 
+            string.Format("");
+
+        }
+
+
+        private static void UpdateLangs()
+        {
+            JsonSerializer serializer = new JsonSerializer();
+
+            using (FileStream fs = File.Open(settings_file, FileMode.Open, 
+                FileAccess.ReadWrite, FileShare.None))
+            using (TextReader tr = new StreamReader(fs))
+            using (TextWriter tw = new StreamWriter(fs)) 
+            using(JsonReader jr = new JsonTextReader(tr))
+            using (JsonWriter jw = new JsonTextWriter(tw)) 
+            {
+
+                while (jr.Read())
+                {
+                    if ((string)jr.Value == "available_langs")
+                    {
+                        jr.Read();
+                        
+                    }
+                }
+
+
+            }
+        }
+              
+        private static void ClearLines(int LinesToClear = 1)
+        {
+            int currentLineCursor = Console.CursorTop - LinesToClear;
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, currentLineCursor);
+        }       
 
         /// <summary>
         /// Returns a [LastName][comma][space][FirstName][period] string
